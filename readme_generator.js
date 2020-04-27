22ddeef62563fa498977ec76ff13d91efbbb2b4c
Alex Hamilton <alexhami916@gmail.com>
2020-04-26 18:53:42 -0600

    Added remainder of selection option inquiries and edited console output coloring and spacing.

--- readme_generator.js
+++ readme_generator.js
@@ -52,16 +52,22 @@ function ListPrompt(name, message, choices, type) {
 const projectNameInput = new InputPrompt("projectName", "What is the name of your project?");
 const licenseInput = new InputPrompt("license", "Please input the license you'd like to use: ");
 const descriptionInput = new InputPrompt("description", "Please enter a description of your project.");
-const addQuestionInput = new InputPrompt("question", "Q:");
-const addAnswerInput = new InputPrompt("answer", "A:");
+const installationInput = new InputPrompt("instructions", "Input an installation instruction block:");
+const installEnvironmentInput = new InputPrompt("environment", "Enter your installation environment:");
+const usageInput = new InputPrompt("instructions", "Enter the usage instructions:");
 const addContributorInput = new InputPrompt("name", "Contributor name:");
 const addContributionInput = new InputPrompt("description", "Contribution to project:");
+const testInput = new InputPrompt("instructions", "Input a test instruction block:");
+const testEnvironmentInput = new InputPrompt("environment", "Enter your test environment:");
+const addQuestionInput = new InputPrompt("question", "Q:");
+const addAnswerInput = new InputPrompt("answer", "A:");
 
-// List Prompts Options
+// Arrays of List Prompt Choices
 const statuses = ["Incomplete", "In Progress", "Ready"];
 const licenses = ["mit", "apache", "zlib", "gpl", "wtfpl", "other", "none"];
 const contentSelections = ["Installation", "Usage", "Contributors", "Tests", "FAQ"];
-const yesOrNo = ["YES", "no"];
+const programingLanguages = ["git", "other"];
+const yesOrNoThankYou = ["YES", "No, thank you."];
 const yesPleaseOrNo = ["Yes, please!", "I'm good, thanks!"];
 
 
@@ -69,22 +75,20 @@ const yesPleaseOrNo = ["Yes, please!", "I'm good, thanks!"];
 const statusList = new ListPrompt("status", "Where is your project currently?", statuses, "list");
 const licenseList = new ListPrompt("license", "What license would you like to use for this project?", licenses, "list");
 const contentSelectionList = new ListPrompt("contents", "What elements would you like to include in your README.md?", contentSelections, "checkbox");
-const addContributorList = new ListPrompt("response", "Would you like to add a contributor to this project?", yesOrNo, "list");
-const addAnotherContributorList = new ListPrompt("response", "Would you like to add another contributor", yesPleaseOrNo, "list");
-const addFAQList = new ListPrompt("response", "Would you like to add FAQs to this README.md?", yesOrNo, "list");
-const addAnotherFAQList = new ListPrompt("response", "Would you like to add another FAQ?", yesPleaseOrNo, "list");
+const installEnvironmentList = new ListPrompt("environment", "Select and installation environment.", programingLanguages, "list");
+const addInstallBlocksList = new ListPrompt("response", "Would you like to add another block of installation instructions?", yesPleaseOrNo, "list");
+const addContributorList = new ListPrompt("response", "Would you like to add another contributor", yesOrNoThankYou, "list");
+const testEnvironmentList = new ListPrompt("environment", "Select and installation environment.", programingLanguages, "list");
+const addTestBlocksList = new ListPrompt("response", "Would you like to add another block of installation instructions?", yesPleaseOrNo, "list");
+const addFAQList = new ListPrompt("response", "Would you like to add another FAQ?", yesPleaseOrNo, "list");
 
 
 // init function
 async function init() {
 
     try {
-        console.log(`
-        
-
-Welcome to the README.md Generator!
-        
-        `);
+        console.log("\x1b[34m%s\x1b[0m", "\nWelcome to the README.md Generator!\n\n");
+        console.log("\x1b[36m%s\x1b[0m", "This application will help you built README file for your projects on the fly.\nAnswer the following prompts accordingly to fill out templated README.md!\n");
 
 
         // PROJECT NAME
@@ -138,68 +142,132 @@ Welcome to the README.md Generator!
 
 
         // Table of Contents
+        console.log("\x1b[34m%s\x1b[0m", "\n\nTable Of Contents");
+        console.log("\x1b[36m%s\x1b[0m", "Now you have the chance to pick which additional\nelements you would like to add to your project README!\n");
         const tableOfContents = await contentSelectionList.ask();
         console.log("table of contents", tableOfContents.contents);
         if(tableOfContents.contents.length > 0) {
             console.log("Contents selected! Count = ", tableOfContents.contents.length);
-            // appendTOCLines(tableOfContents.contents);
+            // await appendTOCLines(tableOfContents.contents);
         }
 
 
-        ["Installation", "Usage", "Contributors", "Tests", "FAQ"]
         // INSTALLATION
+        console.log("\x1b[34m%s\x1b[0m", "\n\nINSTALLATION");
+        console.log("\x1b[36m%s\x1b[0m", "Provide other users with the proper\ninstallation instructions to run your project\n");
+        const installSelect = await installEnvironmentList.ask();
+        let installEnvironment = installSelect.environment;
+        console.log("install environment = ", installEnvironment);
 
+        if(installEnvironment === "other") {
+            const installInput = await installEnvironmentInput.ask();
+            installEnvironment = installInput.environment;
+        }
+
+        let addInstallBlock = "Yes, please!";
+        const installationBlocks = [];
+
+        while(addInstallBlock === "Yes, please!") {
+            let installInstructions = await installationInput.ask();
+            installationBlocks.push(installInstructions.instructions);
+            let continueInstallBlocks = await addInstallBlocksList.ask();
+            addInstallBlock = continueInstallBlocks.response;
+        }
+
+        if(installationBlocks.length > 0) {
+            console.log("Installation instructions recorded!");
+            console.log("install = ", installationBlocks);
+            
+            // await appendInstallationLines(installationBlocks);
+        }
 
         // USAGE
+        console.log("\x1b[34m%s\x1b[0m", "\n\nUSAGE");
+        console.log("\x1b[36m%s\x1b[0m", "The usage section will help users understand the practical execution of your application.\n");
+        const usageDescription = await usageInput.ask();
+        console.log("Usage = ", usageDescription.instructions);
+        if(usageDescription.instructions > 0) {
+            
+            // await appendUsageLines(usageDescription.instructions);
+        }
 
 
         // CONTRIBUTORS
+        console.log("\x1b[34m%s\x1b[0m", "\n\nCONTRIBUTORS");
+        console.log("\x1b[36m%s\x1b[0m", "You will be prompted to add the names of contributors to\nthis project then describe their contributions.\n");
+
         const contributorName = [];
         const contribution = [];
+        let addContributor = "";
 
-        let addContributor = await addContributorList.ask();
 
-        if(addContributor.response === "YES") {
-            while(addContributor.response != "I'm good, thanks!"){
-                let addName = await addContributorInput.ask();
-                contributorName.push(addName.name);
-                let addDescription = await addContributionInput.ask();
-                contribution.push(addDescription.description);
+        while(addContributor.response != "No, thank you."){
+            let addName = await addContributorInput.ask();
+            contributorName.push(addName.name);
+            let addDescription = await addContributionInput.ask();
+            contribution.push(addDescription.description);
 
-                addContributor = await addAnotherContributorList.ask();
-            }
-
-            console.log("Contributors properly saved!");
-            console.log("Contributors = ", contributorName);
-            console.log("Contributions = ", contribution);
-            // await appendContributorsLines(contributorName, contribution);
-
+            addContributor = await addContributorList.ask();
         }
-        
+
+        console.log("Contributors properly saved!");
+        console.log("Contributors = ", contributorName);
+        console.log("Contributions = ", contribution);
+        // await appendContributorsLines(contributorName, contribution);
+
+
         // TESTS
+        console.log("\x1b[34m%s\x1b[0m", "\n\nTESTS");
+        console.log("\x1b[36m%s\x1b[0m", "Provide other users with the proper\n instructions for running tests on your project\n");
+        const testSelect = await testEnvironmentList.ask();
+        let testEnvironment = testSelect.environment;
+        console.log("test environment = ", testEnvironment);
+
+        if(testEnvironment === "other") {
+            const testInput = await testEnvironmentInput.ask();
+            testEnvironment = testInput.environment;
+        }
+
+        let addTestBlock = "Yes, please!";
+        const testBlocks = [];
+
+        while(addTestBlock === "Yes, please!") {
+            let testInstructions = await testInput.ask();
+            testBlocks.push(testInstructions.instructions);
+            let continueTestBlocks = await addTestBlocksList.ask();
+            addTestBlock = continueTestBlocks.response;
+        }
+
+        if(testBlocks.length > 0) {
+            console.log("Test instructions recorded!");
+            console.log("test = ", testBlocks);
+            
+            // await appendTestLines(testBlocks);
+        }
 
 
         // FAQ
+        console.log("\x1b[34m%s\x1b[0m", "\n\nFAQs");
+        console.log("\x1b[36m%s\x1b[0m", "Input a common question about your project,\nthen you will be asked to add an answer.\n");
+
         const questions = [];
         const answers = [];
 
-        let addFAQ = await addFAQList.ask();
+        let addFAQ = "";
 
-        if(addFAQ.response === "YES") {
-            while(addFAQ.response != "I'm good, thanks!"){
-                let addQuestion = await addQuestionInput.ask();
-                questions.push(addQuestion.question);
-                let addAnswer = await addAnswerInput.ask();
-                answers.push(addAnswer.answer);
+        while(addFAQ.response != "I'm good, thanks!"){
+            let addQuestion = await addQuestionInput.ask();
+            questions.push(addQuestion.question);
+            let addAnswer = await addAnswerInput.ask();
+            answers.push(addAnswer.answer);
 
-                addFAQ = await addAnotherFAQList.ask();
-            }
-
-            console.log("Questions and Answers properly saved!");
-            console.log("Questions = ", questions);
-            console.log("answers = ", answers);
-            // await appendFAQLines(questions, answers);
+            addFAQ = await addFAQList.ask();
         }
+
+        console.log("Questions and Answers properly saved!");
+        console.log("Questions = ", questions);
+        console.log("answers = ", answers);
+        // await appendFAQLines(questions, answers);
 
 
     } catch(err) {
